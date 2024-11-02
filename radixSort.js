d29c0a99136877f32e9a9238c775d2953c6c49e1
Osama Salih <osamasalih.ca@gmail.com>
2024-11-02 20:26:37 +0300

    radixSort

---
+++ radixSort.js
@@ -0,0 +1,40 @@
+function getDigit(num, i) {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(arr) {
+  let maxDigits = 0;
+  for (let i = 0; i < arr.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(arr[i]));
+  }
+
+  return maxDigits;
+}
+
+function radixSort(nums) {
+  let maxDigitCount = mostDigits(nums);
+  for (let i = 0; i < maxDigitCount; i++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+    for (let j = 0; j < nums.length; j++) {
+      let digit = getDigit(nums[j], i);
+      digitBuckets[digit].push(nums[j]);
+    }
+    nums = [].concat(...digitBuckets);
+  }
+
+  return nums;
+}
+
+console.log(
+  radixSort([
+    3221, 1, 10, 9680, 577, 9420, 7, 5622, 4793, 2030, 3138, 82, 2599, 743,
+    4127,
+  ])
+);
+
+// Big O (nk)
